d0a3d13e3d19540111e7328a1f2ae67afa578810
Mustafa Turan <mustafaturan.net@gmail.com>
2017-05-02 22:02:27 +0100

    Update version

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -9,8 +9,8 @@
 # Once the dyno has is 'up' you can open your browser and navigate
 # this dyno's directory structure to download the nginx binary.
 
-NGINX_VERSION=${NGINX_VERSION-1.5.7}
-PCRE_VERSION=${PCRE_VERSION-8.21}
+NGINX_VERSION=${NGINX_VERSION-1.12.0}
+PCRE_VERSION=${PCRE_VERSION-8.40}
 HEADERS_MORE_VERSION=${HEADERS_MORE_VERSION-0.23}
 
 nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
